6aee8dade0fa36b1f78b4057722bcc6a649f9739
Eve-code93 <evelynejose1993@gmail.com>
2025-02-21 19:38:28 +0300

    Updated query_samples.py to use .get() and handle exceptions

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,28 +2,32 @@ import os
 import django
 
 # Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-models.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    if author:
+    try:
+        author = Author.objects.get(name=author_name)
         return author.books.all()
-    return []
+    except Author.DoesNotExist:
+        return []
 
 # List all books in a library
 def books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
+    try:
+        library = Library.objects.get(name=library_name)
         return library.books.all()
-    return []
+    except Library.DoesNotExist:
+        return []
 
 # Retrieve the librarian for a library
 def librarian_of_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
+    try:
+        library = Library.objects.get(name=library_name)
         return library.librarian
-    return None
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
+
