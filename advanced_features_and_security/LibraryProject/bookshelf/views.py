82455ae8f05c0faf806ce18568a39faa88a43bb4
Eve-code93 <evelynejose1993@gmail.com>
2025-03-01 18:55:46 +0300

    Add forms.py for handling book data and integrate with views and templates

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -52,3 +52,30 @@ def book_delete(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     # Ensure proper validation and sanitization of user input
     pass
+# LibraryProject/bookshelf/views.py
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/book_form.html', {'form': form})
