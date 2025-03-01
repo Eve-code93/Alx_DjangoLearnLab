061e9ac346b382ff1cbf3162b64e6ceac7049011
Eve-code93 <evelynejose1993@gmail.com>
2025-03-01 18:11:41 +0300

    Customize Django’s user model and update admin integration

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,16 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # Customize the list view
     search_fields = ("title", "author")  # Enable search by title and author
     list_filter = ("publication_year",)  # Add a filter for publication year
+
+
+    # LibraryProject/bookshelf/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
