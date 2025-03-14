05531260a1edb65f22aee5df8cec90eb34f1cbbb
Eve-code93 <evelynejose1993@gmail.com>
2025-03-14 17:10:58 +0300

    Initial setup with models and serializers

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    """
+    Represents an author who has written books.
+    Each author can have multiple books associated with them.
+    """
+    name = models.CharField(max_length=255)
