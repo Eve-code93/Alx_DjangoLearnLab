8f67e6a2ffc3034e031120ef3d2b38db10ae3445
Eve-code93 <evelynejose1993@gmail.com>
2025-03-27 01:29:35 +0300

    fixed serialzers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,16 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # ✅ Ensure CharField is used
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        """Ensure user is created using `create_user` method"""
-        return User.objects.create_user(**validated_data)  # ✅ Fix: Using create_user()
-
+        """Create a new user and generate a token"""
+        user = User.objects.create_user(**validated_data)  # ✅ Using create_user()
+        Token.objects.create(user=user)  # ✅ Generate authentication token
+        return user
