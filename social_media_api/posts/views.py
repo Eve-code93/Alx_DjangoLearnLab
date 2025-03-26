3ea30dabb1c35bb1bb9354198a3eeac7e4dfaa82
Eve-code93 <evelynejose1993@gmail.com>
2025-03-27 01:56:16 +0300

    Added user feed view with posts from followed users

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -33,14 +36,19 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
+CustomUser = get_user_model()
+
 class UserFeedView(generics.ListAPIView):
+    """Generates a feed of posts from followed users"""
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+        following_users = user.following.all()  # Get users the current user follows
+        return Post.objects.filter(author__in=following_users).order_by("-created_at")  # Ensure this line is present
