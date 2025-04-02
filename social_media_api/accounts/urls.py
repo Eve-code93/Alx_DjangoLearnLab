a2ae1357bc793521cb7f14ac9d46d2caf4762529
Eve-code93 <evelynejose1993@gmail.com>
2025-04-02 12:27:47 +0300

    fixed url patterns

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -12,3 +12,9 @@ urlpatterns = [
     path('users/<int:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
     path('users/<int:user_id>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+]
